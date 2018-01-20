240079ab354898a84f8e61f2ef43b66818875cee
Jacob Loukota <jacobloukota@gmail.com>
2018-01-20 15:03:38 -0500

    traffic text is done

--- Law Buddy/TrafficControlView.swift
+++ Law Buddy/TrafficControlView.swift
@@ -7,51 +7,12 @@
 //
 
 import UIKit
-import MapKit
 
-class ViewController: UITableViewController, CLLocationManagerDelegate {
-    
-    let locationManager = CLLocationManager()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        locationManager.requestWhenInUseAuthorization()
-        
-        tableView.alwaysBounceVertical = false
+
+class TrafficControlView: UIViewController{
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.textView.setContentOffset(CGPoint.zero, animated: false)
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        
-    }
-    
-    func getCity() -> String {
-        print("Button tapped")
-        
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation()
-        
-        var city = ""
-        
-        if (CLLocationManager.authorizationStatus() == CLAuthorizationStatus.authorizedWhenInUse) {
-            
-            let geoCoder = CLGeocoder()
-            let location = locationManager.location
-            
-            geoCoder.reverseGeocodeLocation(location!, completionHandler: { (placemarks, error) -> Void in
-                // Place details
-                var placeMark: CLPlacemark!
-                placeMark = placemarks?[0]
-                
-                city = placeMark.locality!
-            })
-        }
-        return formatCity(city)
-    }
-    
-    func formatCity(_ city : String) -> String {
-        return city.lowercased().replacingOccurrences(of: " ", with: "-")
-    }
+
 }
